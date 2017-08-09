2f6763a81f269a35b73db42f3d3e72bd9fc1c0f8
Richard Chau <winterandchaiyun@gmail.com>
2017-08-09 21:18:34 +0800

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -5,11 +5,11 @@ class ResNet(nn.Module):
         self.resnets = list(models.resnet50(pretrained=False).children())
         self.res_o16 = nn.Sequential(*self.resnets[1:-3])
         self.last1 = nn.Sequential(
-            nn.Conv2d(256, 16, 3, padding=1, bias=False),
+            nn.Conv2d(256, 3, 3, padding=1, bias=False),
             nn.ReLU(inplace=True),
             )
         self.last2 = nn.Conv2d(256, 16, 3, padding=1, bias=False) nn.Sequential(
-            nn.Conv2d(256, 16, 3, padding=1, bias=False),
+            nn.Conv2d(256, 3, 3, padding=1, bias=False),
             nn.ReLU(inplace=True),
             )
         
